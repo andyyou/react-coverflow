453fa21626fa5a42e172f15840252c9cc8642894
andyyou <andyyu0920@gmail.com>
2015-10-09 19:11:05 +0800

    Add new example and modify README

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import Coverflow from '../../src/Coverflow';
 
+
 React.render(
-  <Coverflow width="320" height="480"
+  <Coverflow width="960" height="480"
     displayQuantityOfSide={2}
     navigation={false}
     enableHeading={false}
@@ -35,8 +37,40 @@ React.render(
     <img src='images/album-5.png' alt='Album five' url="http://www.bbc.co.uk"/>
     <img src='images/album-6.png' alt='Album six' url="https://medium.com"/>
     <img src='images/album-7.png' alt='Album seven' url="http://www.google.com"/>
-    <img src='images/album-7.png' alt='Album seven' url="http://www.google.com"/>
+
   </Coverflow>,
 
-  document.querySelector('.content')
+  document.querySelector('.example_1')
+);
+
+React.render(
+  <div>
+    <MediaQuery query='(max-width: 960px)'>
+      <Coverflow width="320" height="480"
+        displayQuantityOfSide={2}
+        navigation={false}
+        enableHeading={false}
+        >
+        <img src='images/album-1.png' alt='Album one' url="https://facebook.github.io/react/"/>
+        <img src='images/album-2.png' alt='Album two' url="http://passer.cc"/>
+        <img src='images/album-3.png' alt='Album three' url="https://doce.cc/"/>
+        <img src='images/album-4.png' alt='Album four' url="http://tw.yahoo.com"/>
+      </Coverflow>
+    </MediaQuery>
+    <MediaQuery query='(min-width: 960px)'>
+      <Coverflow width="960" height="480"
+        displayQuantityOfSide={2}
+        navigation={false}
+        enableHeading={false}
+        >
+        <img src='images/album-1.png' alt='Album one' url="https://facebook.github.io/react/"/>
+        <img src='images/album-2.png' alt='Album two' url="http://passer.cc"/>
+        <img src='images/album-3.png' alt='Album three' url="https://doce.cc/"/>
+        <img src='images/album-4.png' alt='Album four' url="http://tw.yahoo.com"/>
+      </Coverflow>
+    </MediaQuery>
+
+  </div>
+  ,
+  document.querySelector('.example_2')
 );
