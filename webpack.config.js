46909ab205de19f2fce155b1871c04dd92595e39
Ariel Salem <Ariel.Salem1989@gmail.com>
2018-09-24 18:15:41 -0700

    Updated / removed dependencies
    In the process of updating dependencies that are no longer supported and updated some necessary dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,64 @@
 const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
-const precss = require('precss');
-const jsloader = (process.env.NODE_ENV === 'react-hot') ? 'react-hot-loader!babel-loader':'babel-loader';
+// const precss = require("precss");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+const jsloader = process.env.NODE_ENV === 'react-hot'
+  ? ['react-hot-loader/babel', 'babel-loader?presets[]=react']
+  : 'babel-loader?presets[]=react';
 const plugins = [
   new webpack.optimize.ModuleConcatenationPlugin(),
   new webpack.LoaderOptionsPlugin({
     debug: true,
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  }),
+  new UglifyJsPlugin(),
   new webpack.HotModuleReplacementPlugin(),
 ];
 
-var entry = {};
-var mainEntryPoints = glob.sync(
+const entry = {};
+const mainEntryPoints = glob.sync(
   // Ignore compile filename with `.bundle.js`
-  path.join(__dirname, './js/!(*.bundle).js')
+  path.join(__dirname, './js/!(*.bundle).js'),
 );
-entry['main'] = mainEntryPoints;
+entry.main = mainEntryPoints;
 
-var config = {
+const config = {
+  mode: 'development',
   context: __dirname,
-  entry: entry,
+  entry,
   output: {
-    path: __dirname + '/js',
+    path: `${__dirname}/js`,
     filename: '[name].bundle.js',
     publicPath: 'js/',
   },
   devtool: 'eval-source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.scss', '.css']
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
+    alias: {
+      radium: require.resolve('radium/index'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        // loader: "babel-loader",
       },
       {
         test: /\.(css|scss)$/,
         loaders: [
           'style-loader',
           'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]',
-          'sass-loader'
+          'sass-loader',
         ],
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      // },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -63,7 +66,7 @@ var config = {
       },
     ],
   },
-  plugins: plugins,
+  plugins,
 };
 
 module.exports = config;
